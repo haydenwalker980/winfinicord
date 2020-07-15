267ad6c1d1a60ab23e0f7dde821b32c27f937372
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2020-07-15 11:12:32 -0700

    Update winfinicord.sh

--- winfinicord.sh
+++ winfinicord.sh
@@ -1,5 +1,6 @@
-echo Launching Winfinicord (https://winfinicord.net)'
-echo Checking for commits to the repository
-echo Updating
-echo Connecting to websocket
-echo Launched!
+echo "Launching Winfinicord (https://winfinicord.net)"
+echo "Checking for commits to the repository"
+echo "Updating"
+echo "Connecting to websocket"
+echo "Launched!"
+xdg-open "https://scratch.mit.edu/projects/410483221/"
